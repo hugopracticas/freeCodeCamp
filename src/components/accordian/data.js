b661c26c61883c23b93fbacae8e70193cd0fbbf0
hugopracticas <hugoloyolamaya@gmail.com>
2024-03-14 23:40:53 -0600

    finish project for accordian

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: 'What are accordion components?',
+        answer: "Accordion components are user interface elements use for"
+    },
+    {
+        id: '2',
+        question: 'What are they used for?',
+        answer: "They are commonly employed in various context, including FAQs,"
+    },
+    {
+        id: '3',
+        question: 'Accordion as a musical instruments',
+        answer: "The accordion is a musical instrument with a keyboard and"
+    },
+    {
+        id: '4',
+        question: 'Can I create an accordion component?',
+        answer: "Yes of course, it is very possible to create an accordion component"
+    },
+]
+
+export default data;
---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import data from "./data"
+import './styles.css'
+
+export default function Accordian(){
+    
+    const [selected, setSelected] = useState(null)
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false)
+    const [multiple, setMultiple] = useState([])
+
+    function handleSingleSelection(getCurrentId){
+        //setSelected(getCurrentId)
+        setSelected(getCurrentId === selected ? null : getCurrentId)
+    }
+
+    return <div className="wrapper">
+        <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enablr Multi Selection</button>
+        <div className="accordian">
+            {
+               data && data.length > 0 ?
+               data.map(dataItem => 
+               <div key={dataItem.id} className="item">
+                    <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
+                    {
+                       selected === dataItem.id ? 
+                       <div className="content">{dataItem.answer}</div>
+                       :null 
+                    }
+               </div>)
+               : <div>No data found</div> 
+            }
+        </div>
+    </div>
+}
